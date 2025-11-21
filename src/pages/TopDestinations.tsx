04cf27573d8a8e4ff950a969e9bc0d5443174271
gokuk <gokulravindran08@gmail.com>
2025-11-22 01:09:03 +0530

    search button placemenet

--- src/pages/TopDestinations.tsx
+++ src/pages/TopDestinations.tsx
@@ -112,7 +112,7 @@ const TopDestinations = () => {
                   />
                   <Button
                     type="submit"
-                    className="absolute right-2 h-12 px-6 bg-[#FF6B00] hover:bg-[#FF5A00] text-white"
+                    className="absolute right-2 top-1/2 transform -translate-y-1/2 h-12 px-6 bg-[#FF6B00] hover:bg-[#FF5A00] text-white"
                   >
                     Search
                   </Button>
